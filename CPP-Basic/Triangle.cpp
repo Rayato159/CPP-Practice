0d882f5559863eb5e50ee9511f24331acab3c711
Rayato159 <rayato159@gmail.com>
2022-04-28 21:17:00 +0700

    -feature :rocket: : Triangle calculator was completed.

---
+++ CPP-Basic/Triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void area() 
+{
+    int b = 7; 
+    int h = 5;
+    float area;
+
+    area=(1.0/2.0)*b*h;
+    cout << area;
+}
+
+int main() {
+    area();
+    return 0;
+}
