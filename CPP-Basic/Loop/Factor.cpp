ac05c09bf0a29e1223e08a32c7157ae1cb211e03
Rayato159 <rayato159@gmail.com>
2022-04-28 23:18:17 +0700

    :rocket: :Factor finder was added.

---
+++ CPP-Basic/Loop/Factor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void factor(int n) {
+    for(int i = 1; i <= n; i++) {
+        if(n%i == 0) {
+            cout << i << " " << flush;
+        } else {
+            continue;
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 8;
+
+    factor(n);
+    return 0;
+}
